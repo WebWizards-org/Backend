a0f284c69723b69fe9f515193376c0a45a0b4016
akshit1535 <158017846+akshit1535@users.noreply.github.com>
2025-08-13 14:05:06 +0530

    add express server and student model for registration

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const StudentModel = require('./models/Students')
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+mongoose.connect("mongodb://127.0.0.1:27017/Students")
+
+app.post('/register', (req, res)=>{
+    StudentModel.create(req.body)
+    .then(Students => res.json(Students))
+    .catch(err => res.json(err))
+})
+
+app.listen(3001, ()=>{
+    console.log("Server is running");
+})
